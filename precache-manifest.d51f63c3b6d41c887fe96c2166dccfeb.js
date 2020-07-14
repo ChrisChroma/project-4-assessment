50c43478b10d4a7de9d3d849dc30f31dee9fe37f
ChrisChroma <chrischroma1030@gmail.com>
2020-07-14 13:49:01 -0500

    Updates

---
+++ precache-manifest.d51f63c3b6d41c887fe96c2166dccfeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca57b909601b04e3ba2982d7d70ddc14",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "021420f0e5c31f672b38",
+    "url": "/project-4-assessment/static/css/main.ad0a2ef1.chunk.css"
+  },
+  {
+    "revision": "ca3003d140765f1cd36a",
+    "url": "/project-4-assessment/static/js/2.cf451cb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.cf451cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021420f0e5c31f672b38",
+    "url": "/project-4-assessment/static/js/main.850966ed.chunk.js"
+  },
+  {
+    "revision": "63b357a442c6bd92d003",
+    "url": "/project-4-assessment/static/js/runtime-main.149457a1.js"
+  }
+]);
